888d1e7975dc05f5e34b620ed8c78434c239fc53
gokulkrishnan2412 <gokulkrishnan2412@gmail.com>
2026-08-29 10:04:37 +0530

    Update Apps Script deployment URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Paste your deployed Google Apps Script web-app URL here.
-window.GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxkkp-0G3la67vcsuINGx4AjM6OeN5QcrY2CbjTJDB8L0uLgD04hUXbLIawwsWgWd6few/exec';
+window.GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwzdgVuX6UZqGALjiRqqYEsAjYrTgDBgHs_cmi9ADFs7zkozW7PXG00LwvqZVDqxbiMUA/exec';
