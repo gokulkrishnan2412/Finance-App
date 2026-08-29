471c48d9971978a94eca9ee21cb04233a261fa6a
gokulkrishnan2412 <gokulkrishnan2412@gmail.com>
2026-08-29 09:42:20 +0530

    config-js-fix

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Paste your deployed Google Apps Script web-app URL here.
-window.GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwA5Us8FM-5bXnF-72_LvYlU43wLtv90Ys3wsE1Xb3qevRwvk86iprum60U9WdLRIK1Fw/exec';
+window.GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxkkp-0G3la67vcsuINGx4AjM6OeN5QcrY2CbjTJDB8L0uLgD04hUXbLIawwsWgWd6few/exec';
